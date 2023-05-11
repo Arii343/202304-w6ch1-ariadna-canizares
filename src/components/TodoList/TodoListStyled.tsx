f4465846e67549f0cb12cf1c05fbe38696ba67b9
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-05-11 08:03:22 +0200

    Add TodoListStyled

---
+++ src/components/TodoList/TodoListStyled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const TodoListStyled = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 10px;
+  padding: 30px;
+`;
+export default TodoListStyled;
